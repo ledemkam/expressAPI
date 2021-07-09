746c1f62b567f7c718319f90530556335ffd22cb
ledemkam <ledemkam@gmail.com>
2021-07-09 08:40:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+const movies = require('./movies');
+const express = require('express');
+const app = express();
+
+const PORT = 3350;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+   res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+   res.sendFile('./views/movies.html', { root: __dirname });
+});
+
+app.get('/movies/api', (req, res) => {
+   res.json(movies);
+   console.log(res.json(movies));
+});
+
+app.get('/movies/api/:year', (req, res) => {
+   const filterByYear = movies.movies.filter(function (year) {
+      return year.year == req.params.year;
+   });
+   res.send(filterByYear);
+});
+
+app.use((req, res) => {
+   res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
+
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
